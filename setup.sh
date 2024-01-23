0c2db5787ef0f731477dc597e7ec7096b3903926
1106601034 <m13712901684@outlook.jp>
2024-01-23 23:34:01 +1100

    This update is processed by a shell script.

--- setup.sh
+++ setup.sh
@@ -5,3 +5,6 @@ npm install typescript-plugin-css-modules --save-dev
 npm install antd@4.24.15 --save
 npm install @ant-design/icons --save
 npm i bootstrap@5.3.2
+cd ..
+mv ./a/node_modules ./
+rm -r ./a
