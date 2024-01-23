44ef7a2e9f7a6ce71f64a9081b1754abe005b83b
1106601034 <m13712901684@outlook.jp>
2024-01-23 21:16:52 +1100

    This update is processed by a shell script.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import styles from './App.module.css';
-
+import { Layout, Typography, Input, } from "antd";
 function App() {
   return (
     <div className = {styles.App}> 
-      <header className={styles['App-header']}>
+      {/* {<header className={styles['App-header']}>
         <img src={logo} className={styles['App-logo']} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +18,14 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header>} */}
+      <div>
+        <Layout.Header>
+          <img src={logo} alt="" />
+          <Typography.Title level={3}>Travel.com</Typography.Title>
+          <Input.Search placeholder='Search for fligghts, hotel and more'/>
+        </Layout.Header>
+      </div>
     </div>
   );
 }
