ab2b1a1759372f790a6ce8f8e36c40e478fa1566
1106601034 <m13712901684@outlook.jp>
2024-01-23 23:00:10 +1100

    This update is processed by a shell script.

--- setup.sh
+++ setup.sh
@@ -2,8 +2,5 @@ npx create-react-app a --template typescript
 cd ./a
 npm install react-icons --save
 npm install typescript-plugin-css-modules --save-dev
-npm install antd --save
+npm install antd@4.24.15 --save
 npm i bootstrap@5.3.2
-cd ..
-mv ./a/node_modules ./
-rm -r ./a
