c1960c36d84c4c663f253c69e7d5d34e928dde04
1106601034 <m13712901684@outlook.jp>
2024-01-25 21:57:28 +1100

    This update is processed by a shell script.

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import styles from './App.module.css';
-import { Layout, Typography, Input, Menu, Button, Dropdown, Space } from "antd";
-import { GlobalOutlined } from "@ant-design/icons";
-
+import { Header, Footer, } from "./components";
 function App() {
   return (
     <div className={styles.App}>
-      {/* top-header */}
-      <div className={styles['top-header']}>
-        <div className={styles.inner}>
-          <Space wrap>
-            <Typography.Text>
-              Explore a world of travel with React.
-            </Typography.Text>
-            <Dropdown menu={{
-              items: [
-                { key: "1", label: "English" },
-                { key: "2", label: "Deutsch" },
-                { key: "3", label: "Francais" },
-              ]
-            }} >
-              <Button style={{ marginLeft: 15 }} >
-                <Space>Language<GlobalOutlined /></Space>
-              </Button>
-            </Dropdown>
-          </Space>
-          <Button.Group className={styles['button-group']}>
-            <Button>Create a free account</Button>
-            <Button>Sign in</Button>
-          </Button.Group>
-        </div>
-      </div>
-      <div className={styles['app-header']}>
-        <Layout.Header className={styles['main-header']}>
-          <img src={logo} alt="logo" className={styles['App-logo']} />
-          <Typography.Title level={3} className={styles.title}>Travel.com</Typography.Title>
-          <Input.Search placeholder='Search for fligghts, hotel and more' className={styles['search-input']} />
-        </Layout.Header>
-        <Menu mode={"horizontal"}
-          className={styles["main-menu"]}
-          items={[
-            { key: 1, label: "Excplore everywhere" },
-            { key: 2, label: "Let us inspire your next trip" },
-            { key: 3, label: "Hotels" },
-            { key: 4, label: "Car hire" },
-          ]} />
-      </div>
-      <Layout.Footer>
-        <Typography.Title level={3} style={{ textAlign: "center" }}>
-          Copyrights @ Travel.com
-        </Typography.Title>
-      </Layout.Footer>
+      <Header />
+      <Footer />
     </div >
   );
 }
