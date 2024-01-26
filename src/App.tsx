a246bf8ac135df827ebec7e8267355e744f6582a
1106601034 <m13712901684@outlook.jp>
2024-01-27 00:44:48 +1100

    This update is processed by a shell script.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
-import styles from './App.module.css';
-import { Header, Footer, SideMenu, Carousel, } from "./components";
-import { Row, Col, } from "antd";
+import React from "react";
+import styles from "./App.module.css";
+import { Header, Footer, Carousel, SideMenu, ProductCollection } from "./components";
+import { Row, Col, Typography } from "antd";
+import { productList1, productList2, productList3 } from "./mockups";
+import sideImage1 from './assets/images/sider_2019_12-09.png';
+import sideImage2 from './assets/images/sider_2019_02-04.png';
+import sideImage3 from './assets/images/sider_2019_02-04-2.png';
 
 function App() {
   return (
@@ -12,6 +16,27 @@ function App() {
           <Col span={6}><SideMenu /></Col>
           <Col span={18}><Carousel /></Col>
         </Row>
+        <ProductCollection 
+          title={<Typography.Title level={3} type='warning'>
+            Big Deal
+          </Typography.Title>}
+          sideImage={sideImage1}
+          products={productList1}
+        />
+        <ProductCollection 
+          title={<Typography.Title level={3} type='danger'>
+            New arrival
+          </Typography.Title>}
+          sideImage={sideImage2}
+          products={productList2}
+        />
+        <ProductCollection 
+          title={<Typography.Title level={3} type='success'>
+            Feature
+          </Typography.Title>}
+          sideImage={sideImage3}
+          products={productList3}
+        />
       </div>
       <Footer />
     </div >
