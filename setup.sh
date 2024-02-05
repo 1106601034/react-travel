d668b1fc2f290846c2743d0670ce936b31946dde
1106601034 <m13712901684@outlook.jp>
2024-02-04 20:00:52 -0500

    This update is processed by a shell script.

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ npm install react-icons --save
 npm install typescript-plugin-css-modules --save-dev
 npm install antd@4.24.15 --save
 npm install @ant-design/icons --save
-npm install bootstrap
-npm install bootstrap-icons
+# npm install bootstrap
+# npm install bootstrap-icons
+npm install react-router-dom
