d6c721f83f48e00f19fc8cd7fc6282a4ae24deb0
1106601034 <m13712901684@outlook.jp>
2024-02-04 21:07:11 -0500

    This update is processed by a shell script.

--- setup.sh
+++ setup.sh
@@ -1,12 +1,17 @@
 # add --save-dev for decrease size of build package
 npx create-react-app a --template typescript
 cd ./a
+
 npm install react-icons --save
+npm install react-router-dom@5
+
+npm install --save @types/react-router-dom --save-dev
 npm install typescript-plugin-css-modules --save-dev
+
 npm install antd@4.24.15 --save
 npm install @ant-design/icons --save
+
 # npm install bootstrap
 # npm install bootstrap-icons
-npm install react-router-dom@5
-npm install --save @types/react-router-dom --save-dev
+
 cd ..
