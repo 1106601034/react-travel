db22313a814c3bc6ae2bd838232ad19991385cc2
1106601034 <m13712901684@outlook.jp>
2024-02-25 22:07:03 +1100

    This update is processed by a shell script.

--- src/components/paymentForm/PaymentForm.tsx
+++ src/components/paymentForm/PaymentForm.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { PaymentInputsWrapper, usePaymentInputs } from "react-payment-inputs";
-import images from "react-payment-inputs/images";
 import { Input, Card } from "antd";
 import styles from "./PaymentForm.module.css";
 
@@ -20,7 +19,6 @@ export const PaymentForm = () => {
       className={styles["payment-credit-card"]}
     >
       <PaymentInputsWrapper {...wrapperProps}>
-        <svg {...getCardImageProps({ images })} />
         <input {...getCardNumberProps()} />
         <input {...getExpiryDateProps()} />
         <input {...getCVCProps()} />
